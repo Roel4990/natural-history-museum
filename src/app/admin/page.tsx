cd55e4652bd5c7bf7f267ffa4ed6235f341af9f6
roel <ash49901@gmail.com>
2025-09-11 02:12:30 +0900

    [MOD] 시간 수정

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,10 +9,15 @@ import {exportToExcel} from "@/app/admin/exportToExcel";
 
 
 function formatTodayISO(): string {
-    const d = new Date();
-    const yyyy = d.getFullYear();
-    const mm = String(d.getMonth() + 1).padStart(2, '0');
-    const dd = String(d.getDate()).padStart(2, '0');
+    const now = new Date();
+
+    // UTC 시간에서 +9시간(KST) 보정
+    const kst = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+
+    const yyyy = kst.getFullYear();
+    const mm = String(kst.getMonth() + 1).padStart(2, '0');
+    const dd = String(kst.getDate()).padStart(2, '0');
+
     return `${yyyy}-${mm}-${dd}`;
 }
 
